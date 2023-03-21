eb835863d330e7a5beb12c01ffdf443d0cbd15da
Emil Lysdahl Fahrenholtz <emillysdahlfahrenholtz@Emils-MBP.lan>
2023-03-21 21:28:23 +0100

    Foreign key specifications and relationships to the models

--- Employee.cs
+++ Employee.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 public class Employee 
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Title { get; set; }
-    public string Department { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Email { get; set; }
-    public Employee Owner { get; set; }
-    public ICollection<Employee> Resources { get; set; }
+    public string? Name { get; set; }
+    public string? Title { get; set; }
+    public string? Department { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+    [ForeignKey(nameof(Owner))]
+    public int? OwnerId { get; set; }
+    [JsonIgnore]
+    public Employee? Owner { get; set; }
+    public ICollection<Employee>? Resources { get; set; }
+    [ForeignKey(nameof(Organization))]
+    public Guid? OrganizationId { get; set; }
+    [JsonIgnore]
+    public Organization? Organization { get; set; }
 }
--- Organization.cs
+++ Organization.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 public class Organization 
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public Employee Owner { get; set; }
+    public string? Name { get; set; }
+    [ForeignKey(nameof(Owner))]
+    public int? OwnerId { get; set; }
+    [JsonIgnore]
+    public Employee? Owner { get; set; }
+    public ICollection<Employee>? Employees { get; set; }
 }
