c3b5c5cb0accf5386fbe59c3b25e3069f915ee0c
Emil Lysdahl Fahrenholtz <emillysdahlfahrenholtz@Emils-MBP.lan>
2023-03-21 21:27:13 +0100

    orginization and employee relationship

--- DataContext.cs
+++ DataContext.cs
@@ -8,4 +8,21 @@ class DataContext : DbContext
     public DbSet<Organization> Organizations => Set<Organization>();
     public DbSet<Employee> Employees => Set<Employee>();
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Organization>(entity =>
+        {
+            entity.HasMany(e => e.Employees)
+                .WithOne(e => e.Organization);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.Property(e => e.Email)
+                .IsRequired();
+            entity.Property(e => e.PhoneNumber)
+                .IsRequired();
+        });
+    }
 }
